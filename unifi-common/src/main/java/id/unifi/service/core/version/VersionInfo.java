a06d7c7590a7bae75f74cf4e533ebdab5a396296
Michal Čudrnák <m@vslice.co.uk>
2017-11-07 17:13:24 +0000

    Output project version & build number on startup

--- unifi-common/src/main/java/id/unifi/service/core/version/VersionInfo.java
+++ unifi-common/src/main/java/id/unifi/service/core/version/VersionInfo.java
@@ -3,7 +3,11 @@ package id.unifi.service.core.version;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.Optional;
+import java.util.Properties;
 
 public class VersionInfo {
     private static final Logger log = LoggerFactory.getLogger(VersionInfo.class);
@@ -18,5 +22,8 @@ public class VersionInfo {
         } else {
             log.info("No Git version information found");
         }
+
+        VersionProperties version = VersionProperties.read();
+        log.info("Project version {} build number {}", version.projectVersion, version.buildNumber);
     }
 }
