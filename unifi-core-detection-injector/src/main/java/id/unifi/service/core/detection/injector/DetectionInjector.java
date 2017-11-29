7520c6e96daa583fa74a14a9b09e4468684a1e62
Michal Čudrnák <m@vslice.co.uk>
2017-11-29 17:18:35 +0000

    Move maven project into 'unifi-service'

--- unifi-core-detection-injector/src/main/java/id/unifi/service/core/detection/injector/DetectionInjector.java
+++
@@ -1,4 +0,0 @@
-package id.unifi.service.core.detection.injector;
-
-public class DetectionInjector {
-}
--- unifi-core-matcher/src/main/java/id/unifi/service/core/matcher/Matcher.java
+++
@@ -1,8 +0,0 @@
-package id.unifi.service.core.matcher;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-public class Matcher {
-    private static final Logger log = LoggerFactory.getLogger(Matcher.class);
-}
