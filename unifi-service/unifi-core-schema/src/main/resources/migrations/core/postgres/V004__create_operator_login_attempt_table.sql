e7786b5f27314d00426e02ce188a4bb0cf4adaa3
Michal Čudrnák <m@vslice.co.uk>
2017-12-20 02:25:12 +0000

    Make login attempt time not null

--- unifi-service/unifi-core-schema/src/main/resources/migrations/core/postgres/V004__create_operator_login_attempt_table.sql
+++ unifi-service/unifi-core-schema/src/main/resources/migrations/core/postgres/V004__create_operator_login_attempt_table.sql
@@ -2,7 +2,7 @@ CREATE TABLE core.operator_login_attempt(
   client_id     CITEXT NOT NULL,
   username      CITEXT NOT NULL,
   successful    BOOLEAN,
-  attempt_time  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  attempt_time  TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
   PRIMARY KEY (client_id, username, attempt_time),
   CONSTRAINT fk_operator_login_attempt_to_operator
