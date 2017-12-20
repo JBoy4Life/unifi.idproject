f1b3f5c0dfd25525071448995cd3c5bb3fb28f73
Justin Megawarne <justin@megawarne.com>
2017-12-20 10:42:18 +0000

    Meagre progress on j-interop wrangling.
    We have a call successfully making it to the Gallagher VM now. But it doesn’t
    actually return a meaningful response yet, due to VM misconfiguration.

--- gallagher-demo/src/main/java/id/unifi/service/demo/gallagher/GallagherDemo.java
+++ gallagher-demo/src/main/java/id/unifi/service/demo/gallagher/GallagherDemo.java
@@ -10,10 +10,10 @@ import com.impinj.octane.Settings;
 import com.impinj.octane.Status;
 import com.impinj.octane.TagReportListener;
 import static java.util.stream.Collectors.toSet;
-import org.jinterop.dcom.core.JIClsid;
-import org.jinterop.dcom.core.JIComServer;
-import org.jinterop.dcom.core.JIProgId;
-import org.jinterop.dcom.core.JISession;
+
+import org.jinterop.dcom.core.*;
+import org.jinterop.dcom.impls.JIObjectFactory;
+import org.jinterop.dcom.impls.automation.IJIDispatch;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -28,22 +28,43 @@ public class GallagherDemo {
     private static final Logger log = LoggerFactory.getLogger(GallagherDemo.class);
 
     public static void main(String[] args) throws Exception {
-        InetAddress localAddress = InetAddress.getLocalHost();
-        //HostAndPort readerEndpoint = findReader(localAddress);
-        HostAndPort readerEndpoint = HostAndPort.fromParts("192.168.42.167", 5084);
 
-        log.info("Found reader {}, connecting...", readerEndpoint);
-        ImpinjReader reader = connectToReader(readerEndpoint);
+//        InetAddress localAddress = InetAddress.getLocalHost();
+//        //HostAndPort readerEndpoint = findReader(localAddress);
+//        HostAndPort readerEndpoint = HostAndPort.fromParts("192.168.42.167", 5084);
+//
+//        log.info("Found reader {}, connecting...", readerEndpoint);
+//        ImpinjReader reader = connectToReader(readerEndpoint);
 
-        String serverAddress = "192.168.42.5";
-        JISession session = JISession.createSession("localhost", "administrator", "MacFUCK69");
-        JIComServer comServer = new JIComServer(JIProgId.valueOf("CardaxFTEIAPI"), serverAddress, session);
-        //JIComServer comServer = new JIComServer(JIProgId.valueOf("CardaxFTCAPI"), serverAddress, session);
-        //JIComServer comServer = new JIComServer(JIClsid.valueOf("70C694E6-8A64-48B0-A58E-A7766C28B7C9"), serverAddress, session);
+        String serverAddress = "192.168.56.2";
+        JISession session = JISession.createSession("localhost", "administrator", "TestPass123");
+        JIComServer ftceapi = new JIComServer(JIClsid.valueOf("C56CA66E-FF0E-4581-B8F3-63F9725D2EC9"), serverAddress, session); // FTCEAPI
 
-        startDetecting(reader, (r, report) -> report.getTags().forEach(t -> {
-            log.info("Detected tag: {}", t);
-        }));
+        IJIComObject ftcApi = ftceapi.createInstance();
+//        IJIComObject ftc  = ftcApi.queryInterface("304B34B7-69F0-4429-B96D-840431E1275C"); // IFTExternalEventInput
+        IJIComObject ftc  = ftcApi.queryInterface("C06D6E49-9FE5-4C1C-8CFE-AA9763CCDDAB"); // IFTExternalEventInput2
+
+//        IJIDispatch dispatch = (IJIDispatch)JIObjectFactory.narrowObject(ftcApi.queryInterface(IJIDispatch.IID));
+//        int opnum = dispatch.getIDsOfNames("logCardEvent");
+
+        JICallBuilder cb = new JICallBuilder(true);
+        cb.setOpnum(1);
+        cb.addInParamAsInt(4, JIFlags.FLAG_NULL);         // lEventType
+        cb.addInParamAsInt(1, JIFlags.FLAG_NULL);         // lEventID
+        cb.addInParamAsDouble(0, JIFlags.FLAG_NULL);      // dEventTime
+        cb.addInParamAsBoolean(false, JIFlags.FLAG_NULL); // bHasRestoral
+        cb.addInParamAsInt(12345, JIFlags.FLAG_NULL);     // lCardNumber
+        cb.addInParamAsInt(11111, JIFlags.FLAG_NULL);     // lFacilityCode
+        cb.addInParamAsString("ES01", JIFlags.FLAG_REPRESENTATION_STRING_BSTR);    // sSystemId
+        cb.addInParamAsString("ESI01", JIFlags.FLAG_REPRESENTATION_STRING_BSTR);   // sItemID
+        cb.addInParamAsString("message", JIFlags.FLAG_REPRESENTATION_STRING_BSTR); // sMessage
+        cb.addInParamAsString("details", JIFlags.FLAG_REPRESENTATION_STRING_BSTR); // sDetails
+        ftc.call(cb);
+
+//        startDetecting(reader, (r, report) -> report.getTags().forEach(t -> {
+//            log.info("Detected tag: {}", t);
+//        }));
+
     }
 
     private static ImpinjReader connectToReader(HostAndPort readerEndpoint) throws OctaneSdkException {
