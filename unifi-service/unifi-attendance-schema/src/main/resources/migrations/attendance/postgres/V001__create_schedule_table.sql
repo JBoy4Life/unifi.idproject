3e3ba478f680109531b3a3499b4c28a8e8535e46
Michal Čudrnák <m@vslice.co.uk>
2017-12-30 04:12:07 +0000

    Attendance schema

---
+++ unifi-service/unifi-attendance-schema/src/main/resources/migrations/attendance/postgres/V001__create_schedule_table.sql
@@ -0,0 +1,11 @@
+CREATE EXTENSION IF NOT EXISTS citext;
+CREATE TABLE attendance.schedule(
+  client_id   CITEXT NOT NULL,
+  schedule_id CITEXT NOT NULL,
+
+  PRIMARY KEY (client_id, schedule_id),
+  CONSTRAINT fk_schedule_to_client
+    FOREIGN KEY (client_id)
+    REFERENCES core.client,
+  CHECK (LENGTH(schedule_id) <= 64)
+);
--- unifi-service/unifi-core-schema/src/main/resources/migrations/core/postgres/V009__create_contact_table.sql
+++ unifi-service/unifi-core-schema/src/main/resources/migrations/core/postgres/V009__create_contact_table.sql
@@ -4,6 +4,7 @@ CREATE TABLE core.contact(
   holder_type      VARCHAR(12) NOT NULL,
 
   PRIMARY KEY (client_id, client_reference, holder_type),
+  UNIQUE (client_id, client_reference),
   CONSTRAINT fk_contact_to_holder
     FOREIGN KEY (client_id, client_reference, holder_type)
     REFERENCES core.holder,
