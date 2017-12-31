f34d70333c8b2fb9ad8e811df1a505d5b1aec574
Justin Megawarne <justin@megawarne.com>
2017-12-31 17:58:17 +0000

    Unifi global styles should be SCSS, not CSS.

--- unifi-web/src/index.js
+++ unifi-web/src/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import './index.css'
-import './unifi.css'
+import './unifi.scss'
 
 import Main from './main'
 import registerServiceWorker from './registerServiceWorker'
