a917387b7b986328eceb2a9defbc8b35347776cb
Michal Čudrnák <m@vslice.co.uk>
2018-01-04 00:33:55 +0000

    get-holder call

--- unifi-service/unifi-core/src/main/java/id/unifi/service/core/services/HolderService.java
+++ unifi-service/unifi-core/src/main/java/id/unifi/service/core/services/HolderService.java
@@ -2,13 +2,17 @@ package id.unifi.service.core.services;
 
 import id.unifi.service.common.api.annotations.ApiOperation;
 import id.unifi.service.common.api.annotations.ApiService;
+import id.unifi.service.common.api.errors.Unauthorized;
 import id.unifi.service.common.db.Database;
 import id.unifi.service.common.db.DatabaseProvider;
 import static id.unifi.service.common.db.DatabaseProvider.CORE_SCHEMA_NAME;
+import id.unifi.service.common.operator.OperatorPK;
 import id.unifi.service.common.operator.OperatorSessionData;
 import static id.unifi.service.core.db.Tables.HOLDER;
+import id.unifi.service.core.db.tables.records.HolderRecord;
 
 import java.util.List;
+import java.util.Optional;
 
 @ApiService("holder")
 public class HolderService {
@@ -20,9 +24,29 @@ public class HolderService {
 
     @ApiOperation
     public List<HolderInfo> listHolders(OperatorSessionData session, String clientId) {
+        authorize(session, clientId);
         return db.execute(sql -> sql.selectFrom(HOLDER)
                 .where(HOLDER.CLIENT_ID.eq(clientId))
-                .fetch(r -> new HolderInfo(r.getClientReference(), r.getName(), r.getHolderType(), r.getActive())));
+                .fetch(HolderService::recordToInfo));
+    }
+
+    @ApiOperation
+    public HolderInfo getHolder(OperatorSessionData session, String clientId, String clientReference) {
+        authorize(session, clientId);
+        return db.execute(sql -> sql.selectFrom(HOLDER)
+                .where(HOLDER.CLIENT_ID.eq(clientId))
+                .and(HOLDER.CLIENT_REFERENCE.eq(clientReference))
+                .fetchOne(HolderService::recordToInfo));
+    }
+
+    private static HolderInfo recordToInfo(HolderRecord r) {
+        return new HolderInfo(r.getClientReference(), r.getName(), r.getHolderType(), r.getActive());
+    }
+
+    private static OperatorPK authorize(OperatorSessionData sessionData, String clientId) {
+        return Optional.ofNullable(sessionData.getOperator())
+                .filter(op -> op.clientId.equals(clientId))
+                .orElseThrow(Unauthorized::new);
     }
 
     public static class HolderInfo {
