81a3015e62265c035edbe6ed0bf98a958c735407
Justin Megawarne <justin@megawarne.com>
2018-01-10 22:48:09 +0000

    Dialogs: ditch the click-to-close.
    It’s causing problems. Fix later.

--- unifi-web/src/components/dialog-box/index.js
+++ unifi-web/src/components/dialog-box/index.js
@@ -5,8 +5,7 @@ import './index.scss'
 export default class DialogBox extends Component {
   render() {
     return (
-      <div className={`unifi-dialog-box ${this.props.visible ? "visible" : ""}`}
-           onClick={() => this.props.onClose()}>
+      <div className={`unifi-dialog-box ${this.props.visible ? "visible" : ""}`}>
         <div className="lightbox">
           {this.props.children}
         </div>
