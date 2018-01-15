5aea0fd60279174fcecba01a2779ad46c0090568
Leo Esaki <leo.esaki3@gmail.com>
2018-01-15 23:28:02 +0900

    Fix scss production build issue

--- unifi-web/config/webpack.config.prod.js
+++ unifi-web/config/webpack.config.prod.js
@@ -146,7 +146,7 @@ module.exports = {
           // use the "style" loader inside the async code so CSS from them won't be
           // in the main CSS file.
           {
-            test: /\.css$/,
+            test: /\s?.css$/,
             loader: ExtractTextPlugin.extract(Object.assign(
               {
                 fallback: {
