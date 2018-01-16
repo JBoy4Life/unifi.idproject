84ad805f9b9a5c2e8a66e93ae7dd7b4f5b56863c
Michal Čudrnák <m@vslice.co.uk>
2018-01-16 16:15:42 +0000

    Add core.client.get-client

--- unifi-service/unifi-core/src/main/java/id/unifi/service/core/services/ClientService.java
+++ unifi-service/unifi-core/src/main/java/id/unifi/service/core/services/ClientService.java
@@ -2,8 +2,11 @@ package id.unifi.service.core.services;
 
 import id.unifi.service.common.api.annotations.ApiOperation;
 import id.unifi.service.common.api.annotations.ApiService;
+import id.unifi.service.common.api.errors.Unauthorized;
 import id.unifi.service.common.db.Database;
 import id.unifi.service.common.db.DatabaseProvider;
+import id.unifi.service.common.operator.OperatorPK;
+import id.unifi.service.common.operator.OperatorSessionData;
 import static id.unifi.service.core.db.Core.CORE;
 import static id.unifi.service.core.db.Tables.CLIENT;
 import static java.util.stream.Collectors.toList;
@@ -11,6 +14,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.List;
+import java.util.Optional;
 
 @ApiService("client")
 public class ClientService {
@@ -30,7 +34,21 @@ public class ClientService {
                 .collect(toList()));
     }
 
-    private static class Client {
+    @ApiOperation
+    public Client getClient(OperatorSessionData session, String clientId) {
+        authorize(session, clientId);
+        return db.execute(sql -> sql.selectFrom(CLIENT)
+                .where()
+                .fetchOne(r -> new Client(r.getClientId(), r.getDisplayName())));
+    }
+
+    private static OperatorPK authorize(OperatorSessionData sessionData, String clientId) {
+        return Optional.ofNullable(sessionData.getOperator())
+                .filter(op -> op.clientId.equals(clientId))
+                .orElseThrow(Unauthorized::new);
+    }
+
+    public static class Client {
         public final String clientId;
         public final String displayName;
 
