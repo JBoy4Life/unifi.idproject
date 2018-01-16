eb1fd73be989849a266cadbda3bb2169812e222b
Leo Esaki <leo.esaki3@gmail.com>
2018-01-17 00:58:35 +0900

    Make filterItems fail-safe

--- unifi-web/src/pages/live-view/directory/utils/helpers.js
+++ unifi-web/src/pages/live-view/directory/utils/helpers.js
@@ -18,8 +18,8 @@ export const groupItems = (items, grouping) => {
 export const filterItems = (items, filters, search) =>
   items.filter((item) => {
     const matchesFilters = filters
-      .reduce((acc, filter) => acc || item.client.holderType === filter, filters.length === 0)
-    const matchesSearch = item.client.name ? item.client.name.indexOf(search) !== -1 : true
+      .reduce((acc, filter) => acc || item.client && item.client.holderType === filter, filters.length === 0)
+    const matchesSearch = item.client && item.client.name ? item.client.name.indexOf(search) !== -1 : true
     return matchesFilters && matchesSearch
   })
 
