c3da3d470e6ee16f17de05aa5cfa8a69633d1ce0
Leo Esaki <leo.esaki3@gmail.com>
2018-01-17 18:47:00 +0900

    Re-organize folder structure for scss variables, mixins, resets

--- unifi-web/src/index.js
+++ unifi-web/src/index.js
@@ -1,8 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import './index.css'
-import './unifi.scss'
+import './styles/index.scss'
 
 import Main from './main'
 import registerServiceWorker from './registerServiceWorker'
