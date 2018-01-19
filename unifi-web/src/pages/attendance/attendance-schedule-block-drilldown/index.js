64840c9fee603083c95e8012b7db17360206b120
Leo Esaki <leo.esaki3@gmail.com>
2018-01-20 05:56:26 +0900

    Fix percentage, include client reference, revamp & clean up codes

--- unifi-web/src/pages/attendance/attendance-schedule-block-drilldown/index.js
+++ unifi-web/src/pages/attendance/attendance-schedule-block-drilldown/index.js
@@ -38,69 +38,58 @@ const sortBlockReport = fp.compose(
   }))
 )
 
+const getPercentage = (blockReport) => {
+  const presentCount = fp.compose(
+    fp.size,
+    fp.filter({ status: 'present' })
+  )(blockReport)
+  const processedCount = fp.compose(
+    fp.size,
+    fp.reject({ status: null })
+  )(blockReport)
+  return presentCount / (processedCount || 1) * 100
+}
+
+const singleScheduleSelector = (state, props) =>
+  fp.compose(
+    fp.get('[0]'),
+    fp.filter({ scheduleId: props.match.params.scheduleId }),
+    fp.defaultTo([]),
+    schedulesSelector
+  )(state)
+
+const commiterSelector = (state, props) =>
+  fp.compose(
+    fp.find({ clientReference: props.match.params.clientReference }),
+    fp.defaultTo([]),
+    contactAttendanceSelector
+  )(state)
+
 export class AttendanceScheduleBlockDrilldown extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      committer: {
-        name: ''
-      },
-      schedule: {
-        name: '',
-        attendance: 0,
-        startDate: null,
-        endDate: null,
-        committerCount: 0,
-        blockCount: 0,
-      },
       editAttendanceVisible: false,
       editAttendanceSelectedStatus: null,
       editAttendanceBlockId: null
     }
   }
 
-  componentWillReceiveProps(nextProps) {
-    if (nextProps.scheduleStats.length > 0) {
-      let scheduleId = nextProps.match.params.scheduleId
-      let schedule = nextProps.scheduleStats.filter((schedule) => schedule.scheduleId === scheduleId)[0]
-      let percentage = (schedule.blockCount === 0 || schedule.committerCount === 0) ? 0 :
-        (schedule.overallAttendance / (schedule.committerCount * schedule.blockCount)) * 100
+  componentWillMount() {
+    const { clientReference, scheduleId } = this.props.match.params
+    this.loadData(clientReference, scheduleId)
+  }
 
-      this.setState({
-        schedule: {
-          name: schedule.name,
-          attendance: percentage,
-          startDate: schedule.startTime,
-          endDate:   schedule.endTime,
-          committerCount: schedule.committerCount,
-          blockCount: schedule.blockCount
-        }
-      })
-    }
-    if (nextProps.contactAttendance.attendance) {
-      let contact = nextProps.contactAttendance.attendance.find((r) => r.clientReference === this.props.match.params.clientReference)
-      if (contact) {
-        this.setState({
-          committer: {
-            name: contact.name
-          }
-        })
-      }
-    }
+  componentWillReceiveProps(nextProps) {
     if (nextProps.overrideAttendanceResult !== this.props.overrideAttendanceResult &&
       this.props.overrideAttendanceResult !== null) {
       // Gotta refresh.
-      const scheduleId = this.props.match.params.scheduleId;
-      const clientReference = this.props.match.params.clientReference;
-      this.props.listScheduleStats(scheduleId);
-      this.props.reportBlockAttendance(scheduleId, clientReference);
-      this.props.getContactAttendanceForSchedule(scheduleId);
+      const { clientReference, scheduleId } = this.props.match.params
+      this.loadData(clientReference, scheduleId)
     }
   }
 
-  componentWillMount() {
-    const scheduleId = this.props.match.params.scheduleId
-    const clientReference = this.props.match.params.clientReference
+  loadData(clientReference, scheduleId) {
     this.props.listScheduleStats(scheduleId)
     this.props.reportBlockAttendance(scheduleId, clientReference)
     this.props.getContactAttendanceForSchedule(scheduleId)
@@ -110,45 +99,46 @@ export class AttendanceScheduleBlockDrilldown extends Component {
     this.setState({
       editAttendanceVisible: true,
       editAttendanceBlockId: blockId
-    });
+    })
   }
 
-  onEditAttendanceCancel = () => {
+  handleEditAttendanceCancel = () => {
     this.setState({
       editAttendanceVisible: false
-    });
+    })
   }
 
-  onEditAttendanceSave = () => {
+  handleEditAttendanceSave = () => {
     this.props.overrideAttendance(
       this.props.match.params.clientReference,
       this.props.match.params.scheduleId,
       this.state.editAttendanceBlockId,
-      this.state.editAttendanceSelectedStatus);
+      this.state.editAttendanceSelectedStatus)
     this.setState({
       editAttendanceVisible: false
-    });
+    })
   }
 
   handleNewAttendanceSelect = (key) => {
     this.setState({
       editAttendanceSelectedStatus: key
-    });
+    })
   }
 
   handleNewAttendanceClear = () => {
     this.setState({
       editAttendanceSelectedStatus: null
-    });
+    })
   }
 
   render() {
-    const { blockReport } = this.props
-    const { committer, schedule } = this.state
+    const { blockReport, committer, schedule } = this.props
+    const sortedBlockReport = sortBlockReport(blockReport)
+    const { clientReference } = this.props.match.params
 
     return (
       <div className="attendanceScheduleBlockDrilldown">
-        {this.state.editAttendanceVisible ?
+        {this.state.editAttendanceVisible &&
           <DialogBox>
             <h1>Edit Attendance</h1>
             <SearchableSelectField 
@@ -162,30 +152,38 @@ export class AttendanceScheduleBlockDrilldown extends Component {
               <button
                 className="unifi-button primary"
                 disabled={this.editAttendanceSelectedStatus ? "disabled" : ""}
-                onClick={this.onEditAttendanceSave}
+                onClick={this.handleEditAttendanceSave}
               >
                 Save
               </button>
               <button
                 className="unifi-button"
-                onClick={this.onEditAttendanceCancel}
+                onClick={this.handleEditAttendanceCancel}
               >
                 Cancel
               </button>
             </div>
           </DialogBox>
-        :
-          ""}
-        <h1>{committer.name}</h1>
-        <h2>{schedule.name}</h2>
+        }
+        <h1>{committer && committer.name}</h1>
+        <h2>{schedule && schedule.name}</h2>
         <div className="schedule-stats-summary">
-          <EvacuationProgressBar percentage={Math.floor(schedule.attendance)} warningThreshold={80} criticalThreshold={50} />
+          <EvacuationProgressBar
+            percentage={(getPercentage(sortedBlockReport))}
+            warningThreshold={80}
+            criticalThreshold={50}
+          />
           <p className="label">Overall Attendance</p>
           <div className="stats">
-            <p className="stat"><span>Lectures:</span>{' '}{blockReport.length}</p>
+            <p className="stat"><span>Client Reference:</span> {clientReference}</p>
+            <p className="stat"><span>Lectures:</span> {blockReport.length}</p>
             <br />
-            <p className="stat"><span>Present:</span>{' '}{blockReport.filter((block) => block.status === "present").length}</p>
-            <p className="stat"><span>Absent:</span>{' '}{blockReport.filter((block) => block.status === "absent").length}</p>
+            <p className="stat">
+              <span>Present:</span> {blockReport.filter((block) => block.status === "present").length}
+            </p>
+            <p className="stat">
+              <span>Absent:</span> {blockReport.filter((block) => block.status === "absent").length}
+            </p>
           </div>
         </div>
         <div className="views">
@@ -202,10 +200,12 @@ export class AttendanceScheduleBlockDrilldown extends Component {
             </tr>
           </thead>
           <tbody>
-            {sortBlockReport(blockReport).map((block) => (
+            {sortedBlockReport.map((block) => (
               <tr key={block.blockId}>
                 <td className="times">
-                  {block.startTime.format('DD/MM/Y')}, {block.startTime.format('HH:mm')}–{block.endTime.format('HH:mm')}
+                  {block.startTime.format('DD/MM/Y')},
+                  {' '}
+                  {block.startTime.format('HH:mm')} – {block.endTime.format('HH:mm')}
                 </td>
                 <td className="status">
                   {absenceLabels[block.status]}
@@ -228,9 +228,10 @@ export class AttendanceScheduleBlockDrilldown extends Component {
 
 const selector = createStructuredSelector({
   blockReport: blockReportSelector,
-  contactAttendance: contactAttendanceSelector,
   overrideAttendanceResult: overrideAttendanceResultSelector,
-  scheduleStats: schedulesSelector
+  scheduleStats: schedulesSelector,
+  schedule: singleScheduleSelector,
+  commiter: commiterSelector
 })
 
 const actions = {
