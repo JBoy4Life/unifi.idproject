7373913bea44a5cf0485006d1aa4e259113ff325
Michal Čudrnák <m@vslice.co.uk>
2018-01-19 02:13:26 +0000

    Noddy config option to dump detections into a file

--- unifi-service/unifi-core-agent/src/main/java/id/unifi/service/core/agent/CoreAgentService.java
+++ unifi-service/unifi-core-agent/src/main/java/id/unifi/service/core/agent/CoreAgentService.java
@@ -1,37 +1,30 @@
 package id.unifi.service.core.agent;
 
-import com.google.common.collect.Iterables;
 import com.statemachinesystems.envy.Default;
 import com.statemachinesystems.envy.Envy;
+import com.statemachinesystems.envy.Nullable;
 import com.statemachinesystems.envy.Prefix;
 import id.unifi.service.common.api.ComponentHolder;
 import id.unifi.service.common.config.HostAndPortValueParser;
 import id.unifi.service.common.config.UnifiConfigSource;
-import id.unifi.service.common.db.Database;
 import id.unifi.service.common.db.DatabaseProvider;
-import id.unifi.service.common.detection.DetectableType;
-import id.unifi.service.common.detection.RawDetection;
 import id.unifi.service.common.detection.RawDetectionReport;
-import static id.unifi.service.core.db.Core.CORE;
-import static id.unifi.service.core.db.Tables.ANTENNA;
-import static id.unifi.service.core.db.Tables.DETECTABLE;
-import id.unifi.service.core.db.tables.records.AntennaRecord;
-import id.unifi.service.core.db.tables.records.DetectableRecord;
 import id.unifi.service.provider.rfid.RfidProvider;
-import static java.util.stream.Collectors.groupingBy;
-import static java.util.stream.Collectors.toList;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.BufferedWriter;
+import java.io.IOException;
 import java.net.URI;
-import java.time.Instant;
-import java.util.Arrays;
-import java.util.List;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.Map;
-import java.util.Random;
+import java.util.Optional;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.Consumer;
-import java.util.stream.IntStream;
 
 public class CoreAgentService {
     private static final Logger log = LoggerFactory.getLogger(CoreAgentService.class);
@@ -49,13 +42,41 @@ public class CoreAgentService {
 
         @Default("false")
         boolean mockDetections();
+
+        @Nullable
+        String detectionLogFilePath();
     }
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         Config config = Envy.configure(Config.class, UnifiConfigSource.get(), HostAndPortValueParser.instance);
 
+        Optional<BufferedWriter> logWriter = Optional.ofNullable(config.detectionLogFilePath()).flatMap(filePath -> {
+            try {
+                Path path = Paths.get(filePath);
+                log.info("Appending detections to {}", path);
+                return Optional.of(Files.newBufferedWriter(
+                        path,
+                        StandardCharsets.UTF_8,
+                        StandardOpenOption.WRITE,
+                        StandardOpenOption.APPEND,
+                        StandardOpenOption.CREATE));
+            } catch (IOException e) {
+                log.error("Error opening detection log file", e);
+                return Optional.empty();
+            }
+        });
+
         AtomicReference<CoreClient> client = new AtomicReference<>();
-        Consumer<RawDetectionReport> detectionConsumer = report -> client.get().sendRawDetections(report);
+        Consumer<RawDetectionReport> detectionConsumer = report -> {
+            client.get().sendRawDetections(report);
+            logWriter.ifPresent(w -> {
+                try {
+                    w.write(report.toString());
+                    w.newLine();
+                    w.flush();
+                } catch (IOException ignored) {}
+            });
+        };
         ReaderManager readerManager = config.mockDetections()
                 ? new MockReaderManager(new DatabaseProvider(), config.clientId(), config.siteId(), detectionConsumer)
                 : new DefaultReaderManager(new DatabaseProvider(), new RfidProvider(detectionConsumer));
