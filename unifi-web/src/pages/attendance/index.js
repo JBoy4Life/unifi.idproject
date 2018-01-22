02482672b5a3f3f1fd262a98602d9b2c75fe37a2
Leo Esaki <leo.esaki3@gmail.com>
2018-01-22 11:31:23 +0900

    Cleanup code in attendance site container

--- unifi-web/src/pages/attendance/index.js
+++ unifi-web/src/pages/attendance/index.js
@@ -1,73 +1,70 @@
-import React, { Component } from 'react';
-import { Route, Switch /* Redirect */ } from 'react-router';
+import React from 'react'
+import { Route, Switch /*, Redirect*/ } from 'react-router'
 
-import * as ROUTES from '../../utils/routes';
+import * as ROUTES from 'utils/routes'
+import { PageContainer, LinkedSideNavigation } from 'smart-components'
+import { PageContent } from 'components'
 
-import { PageContent } from '../../components';
-import { PageContainer, LinkedSideNavigation } from '../../smart-components';
+import AttendanceCustomReports from './attendance-custom-reports'
+import AttendanceReports from './attendance-reports'
+import AttendanceScheduleBlockDrilldown from './attendance-schedule-block-drilldown'
+import AttendanceScheduleDetail from './attendance-schedule-detail'
+import AttendanceSchedules from './attendance-schedules'
 
-import AttendanceSchedules from './attendance-schedules';
-import AttendanceScheduleDetail from './attendance-schedule-detail';
-import AttendanceScheduleBlockDrilldown from './attendance-schedule-block-drilldown';
-import AttendanceReports from './attendance-reports';
-import AttendanceCustomReports from './attendance-custom-reports';
+const menus = [
+  {
+    key: '/attendance/schedules',
+    icon: 'calendar',
+    label: 'Modules',
+  },
+  {
+    key: '/attendance/reports',
+    icon: 'bar-chart',
+    label: 'Reports',
+  },
+  // {
+  //   key: '/attendance/custom-reports',
+  //   label: 'Custom Reports',
+  // }
+]
 
-const menus = [{
-  key: '/attendance/schedules',
-  icon: 'calendar',
-  label: 'Modules',
-},
-{
-  key: '/attendance/reports',
-  icon: 'bar-chart',
-  label: 'Reports',
-},
-// {
-//   key: '/attendance/custom-reports',
-//   label: 'Custom Reports',
-// }
-];
+const SitemapContainer = () => (
+  <PageContainer>
+    <PageContent>
+      <PageContent.Sidebar>
+        <LinkedSideNavigation menus={menus} />
+      </PageContent.Sidebar>
+      <PageContent.Main>
+        <Switch>
+          <Route
+            exact
+            path={ROUTES.ATTENDANCE_SCHEDULES}
+            component={AttendanceSchedules}
+          />
+          <Route
+            exact
+            path={ROUTES.ATTENDANCE_SCHEDULES_DETAIL}
+            component={AttendanceScheduleDetail}
+          />
+          <Route
+            exact
+            path={ROUTES.ATTENDANCE_SCHEDULES_BLOCK_DRILLDOWN}
+            component={AttendanceScheduleBlockDrilldown}
+          />
+          <Route
+            exact
+            path={ROUTES.ATTENDANCE_REPORTS}
+            component={AttendanceReports}
+          />
+          {/* <Route
+            exact
+            path={ROUTES.ATTENDANCE_CUSTOM_REPORTS}
+            component={AttendanceCustomReports}
+          /> */}
+        </Switch>
+      </PageContent.Main>
+    </PageContent>
+  </PageContainer>
+)
 
-export default class SitemapContainer extends Component {
-  render() {
-    return (
-      <PageContainer>
-        <PageContent>
-          <PageContent.Sidebar>
-            <LinkedSideNavigation menus={menus} />
-          </PageContent.Sidebar>
-          <PageContent.Main>
-            <Switch>
-              <Route
-                exact
-                path={ROUTES.ATTENDANCE_SCHEDULES}
-                component={AttendanceSchedules}
-              />
-              <Route
-                exact
-                path={ROUTES.ATTENDANCE_SCHEDULES_DETAIL}
-                component={AttendanceScheduleDetail}
-              />
-              <Route
-                exact
-                path={ROUTES.ATTENDANCE_SCHEDULES_BLOCK_DRILLDOWN}
-                component={AttendanceScheduleBlockDrilldown}
-              />
-              <Route
-                exact
-                path={ROUTES.ATTENDANCE_REPORTS}
-                component={AttendanceReports}
-              />
-              {/*<Route*/}
-                {/*exact*/}
-                {/*path={ROUTES.ATTENDANCE_CUSTOM_REPORTS}*/}
-                {/*component={AttendanceCustomReports}*/}
-              {/*/>*/}
-            </Switch>
-          </PageContent.Main>
-        </PageContent>
-      </PageContainer>
-    )
-  }
-}
-
+export default SitemapContainer
