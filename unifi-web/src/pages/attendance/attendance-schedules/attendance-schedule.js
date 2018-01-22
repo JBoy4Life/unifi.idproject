c15f3a0c98494b5d3350e4be796e21f7c960bafe
Leo Esaki <leo.esaki3@gmail.com>
2018-01-22 11:26:07 +0900

    Show TBD in attendance-schedule && add prop-type checking

--- unifi-web/src/pages/attendance/attendance-schedules/attendance-schedule.js
+++ unifi-web/src/pages/attendance/attendance-schedules/attendance-schedule.js
@@ -1,39 +1,60 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React, { Component } from 'react'
+import moment from "moment"
+import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
 
-import EvacuationProgressBar from "../../../components/evacuation-progress-bar";
-import moment from "moment";
+import EvacuationProgressBar from "../../../components/evacuation-progress-bar"
 
-import './attendance-schedule.scss';
+import './attendance-schedule.scss'
 
 export default class AttendanceSchedule extends Component {
+  static propTypes = {
+    attendance: PropTypes.number,
+    blockCount: PropTypes.number,
+    committerCount: PropTypes.number,
+    endDate: PropTypes.string,
+    processedBlockCount: PropTypes.number,
+    scheduleId: PropTypes.number,
+    startDate: PropTypes.string,
+    title: PropTypes.string
+  }
+
   render() {
-    let percentage = (this.props.processedBlockCount === 0 || this.props.committerCount === 0) ? 0 :
-      Math.floor((this.props.attendance / (this.props.committerCount * this.props.processedBlockCount)) * 100);
-    let startDate = moment(this.props.startDate).format('DD/MM/Y');
-    let endDate   = moment(this.props.endDate).format('DD/MM/Y');
+    const { attendance, blockCount, committerCount, processedBlockCount,
+      scheduleId, startDate, endDate, title } = this.props
+    const processedCount = committerCount * processedBlockCount
+    const percentage = Math.round((attendance / (processedCount || 1)) * 100)
+
+    const startDateF = moment(startDate).format('DD/MM/Y')
+    const endDateF = moment(endDate).format('DD/MM/Y')
+
     return (
-        <div className="attendanceSchedule">
-          <Link to={`/attendance/schedules/${this.props.scheduleId}`}>
-            <div className="title">
-              <h2>{this.props.title}</h2>
+      <div className="attendanceSchedule">
+        <Link to={`/attendance/schedules/${scheduleId}`}>
+          <div className="title">
+            <h2>{title}</h2>
+          </div>
+          <div className="schedule-stats-summary">
+            <EvacuationProgressBar
+              percentage={percentage}
+              warningThreshold={90}
+              criticalThreshold={70}
+              tbd={!processedCount}
+            />
+            <p className="label">Overall Attendance to Date</p>
+            <div className="stats">
+              {(startDate === null) ? (
+                <p className="stat"><span>Dates: </span>Unscheduled</p>
+              ) : (
+                <p className="stat"><span>Dates: </span>{startDateF} – {endDateF}</p>
+              )}
+              <br />
+              <p className="stat"><span>Students: </span>{committerCount}</p>
+              <p className="stat"><span>Lectures: </span>{blockCount}</p>
             </div>
-            <div className="schedule-stats-summary">
-              <EvacuationProgressBar percentage={percentage} warningThreshold={90} criticalThreshold={70} />
-              <p className="label">Overall Attendance to Date</p>
-              <div className="stats">
-                {(this.props.startDate === null) ?
-                  <p className="stat"><span>Dates:</span>&nbsp;Unscheduled</p>
-                  :
-                  <p className="stat"><span>Dates:</span>&nbsp;{startDate} – {endDate}</p>
-                }
-                <br />
-                <p className="stat"><span>Students:</span>&nbsp;{this.props.committerCount}</p>
-                <p className="stat"><span>Lectures:</span>&nbsp;{this.props.blockCount}</p>
-              </div>
-            </div>
-          </Link>
-        </div>
-    );
+          </div>
+        </Link>
+      </div>
+    )
   }
 }
