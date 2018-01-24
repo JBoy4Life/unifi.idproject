2a417f8ea665675b9a96d420a63bd43a1d74923e
Justin Megawarne <justin@megawarne.com>
2018-01-24 23:56:59 +0000

    Finally something that works.

--- gallagher-demo/src/main/java/id/unifi/service/demo/gallagher/GallagherDemo.java
+++ gallagher-demo/src/main/java/id/unifi/service/demo/gallagher/GallagherDemo.java
@@ -13,7 +13,6 @@ import static java.util.stream.Collectors.toSet;
 
 import org.jinterop.dcom.core.*;
 import org.jinterop.dcom.impls.JIObjectFactory;
-import org.jinterop.dcom.impls.automation.IJIDispatch;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -21,13 +20,20 @@ import javax.jmdns.JmDNS;
 import javax.jmdns.ServiceInfo;
 import java.io.IOException;
 import java.net.InetAddress;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Set;
+import java.util.concurrent.CountDownLatch;
 
 public class GallagherDemo {
+
     private static final Logger log = LoggerFactory.getLogger(GallagherDemo.class);
+    public static final CountDownLatch registerLatch = new CountDownLatch(1);
+    private static final CountDownLatch quitLatch = new CountDownLatch(1);
 
     public static void main(String[] args) throws Exception {
+
+        final String GUID_FTCEAPI = "C56CA66E-FF0E-4581-B8F3-63F9725D2EC9";
 
 //        InetAddress localAddress = InetAddress.getLocalHost();
 //        //HostAndPort readerEndpoint = findReader(localAddress);
@@ -38,33 +44,128 @@ public class GallagherDemo {
 
         String serverAddress = "192.168.56.2";
         JISession session = JISession.createSession("localhost", "administrator", "TestPass123");
-        JIComServer ftceapi = new JIComServer(JIClsid.valueOf("C56CA66E-FF0E-4581-B8F3-63F9725D2EC9"), serverAddress, session); // FTCEAPI
+        JIComServer comServer = new JIComServer(JIClsid.valueOf(GUID_FTCEAPI), serverAddress, session);
+        IJIComObject ftcApi = comServer.createInstance();
 
-        IJIComObject ftcApi = ftceapi.createInstance();
-//        IJIComObject ftc  = ftcApi.queryInterface("304B34B7-69F0-4429-B96D-840431E1275C"); // IFTExternalEventInput
-        IJIComObject ftc  = ftcApi.queryInterface("C06D6E49-9FE5-4C1C-8CFE-AA9763CCDDAB"); // IFTExternalEventInput2
+        JILocalCoClass middleware = createMiddleware();
+        registerMiddleware(ftcApi, middleware);
 
-//        IJIDispatch dispatch = (IJIDispatch)JIObjectFactory.narrowObject(ftcApi.queryInterface(IJIDispatch.IID));
-//        int opnum = dispatch.getIDsOfNames("logCardEvent");
+//        startDetecting(reader, (r, report) -> report.getTags().forEach(t -> {
+//            log.info("Detected tag: {}", t);
+//        }));
 
+        registerLatch.await();
+
+        IJIComObject ftc = ftcApi.queryInterface("C06D6E49-9FE5-4C1C-8CFE-AA9763CCDDAB");
+        logCardEvent(ftc);
+
+        quitLatch.await();
+
+    }
+
+
+    private static JILocalCoClass createMiddleware() {
+
+        final JILocalCoClass coClass = new JILocalCoClass(
+                new JILocalInterfaceDefinition(Middleware.CLSID, false),
+                Middleware.class);
+
+        JILocalInterfaceDefinition interfaceDefinition =
+                coClass.getInterfaceDefinition();
+        interfaceDefinition.addMethodDescriptor(notifyItemRegisteredMD());
+        interfaceDefinition.addMethodDescriptor(notifyItemDeregisteredMD());
+        interfaceDefinition.addMethodDescriptor(notifySystemRegisteredMD());
+        interfaceDefinition.addMethodDescriptor(notifySystemDeregisteredMD());
+        interfaceDefinition.addMethodDescriptor(notifyAlarmAcknowledgedMD());
+
+        ArrayList<String> supportedInterfaces = new ArrayList<>();
+        supportedInterfaces.add("ED5CDD41-4393-4B88-BF30-F250905D57C8");
+        coClass.setSupportedEventInterfaces(supportedInterfaces);
+
+        return coClass;
+
+    }
+
+    private static String registerMiddleware(
+            IJIComObject ftc,
+            JILocalCoClass middleware
+    ) throws Exception {
+
+        IJIComObject remoteMiddleware = ftc.queryInterface(
+                "304B34B7-69F0-4429-B96D-840431E1275C");
+        return JIObjectFactory.attachEventHandler(
+                remoteMiddleware,
+                "ED5CDD41-4393-4B88-BF30-F250905D57C8",
+                JIObjectFactory.buildObject(ftc.getAssociatedSession(), middleware));
+
+    }
+
+    private static JILocalMethodDescriptor notifyItemRegisteredMD() {
+        final JILocalParamsDescriptor params = new JILocalParamsDescriptor();
+        params.addInParamAsObject(new JIString(JIFlags.FLAG_REPRESENTATION_STRING_BSTR), JIFlags.FLAG_NULL);
+        params.addInParamAsObject(new JIString(JIFlags.FLAG_REPRESENTATION_STRING_BSTR), JIFlags.FLAG_NULL);
+        params.addInParamAsObject(new JIString(JIFlags.FLAG_REPRESENTATION_STRING_BSTR), JIFlags.FLAG_NULL);
+        return new JILocalMethodDescriptor("notifyItemRegistered", params);
+    }
+
+    private static JILocalMethodDescriptor notifyItemDeregisteredMD() {
+        final JILocalParamsDescriptor params = new JILocalParamsDescriptor();
+        params.addInParamAsObject(new JIString(JIFlags.FLAG_REPRESENTATION_STRING_BSTR), JIFlags.FLAG_NULL);
+        params.addInParamAsObject(new JIString(JIFlags.FLAG_REPRESENTATION_STRING_BSTR), JIFlags.FLAG_NULL);
+        return new JILocalMethodDescriptor("notifyItemDeregistered", params);
+    }
+
+    private static JILocalMethodDescriptor notifySystemRegisteredMD() {
+        final JILocalParamsDescriptor params = new JILocalParamsDescriptor();
+        params.addInParamAsObject(new JIString(JIFlags.FLAG_REPRESENTATION_STRING_BSTR), JIFlags.FLAG_NULL);
+        params.addInParamAsObject(new JIString(JIFlags.FLAG_REPRESENTATION_STRING_BSTR), JIFlags.FLAG_NULL);
+        params.addInParamAsObject(new JIString(JIFlags.FLAG_REPRESENTATION_STRING_BSTR), JIFlags.FLAG_NULL);
+        return new JILocalMethodDescriptor("notifySystemRegistered", params);
+    }
+
+    private static JILocalMethodDescriptor notifySystemDeregisteredMD() {
+        final JILocalParamsDescriptor params = new JILocalParamsDescriptor();
+        params.addInParamAsObject(new JIString(JIFlags.FLAG_REPRESENTATION_STRING_BSTR), JIFlags.FLAG_NULL);
+        return new JILocalMethodDescriptor("notifySystemDeregistered", params);
+    }
+
+    private static JILocalMethodDescriptor notifyAlarmAcknowledgedMD() {
+        final JILocalParamsDescriptor params = new JILocalParamsDescriptor();
+        params.addInParamAsObject(new JIString(JIFlags.FLAG_REPRESENTATION_STRING_BSTR), JIFlags.FLAG_NULL);
+        params.addInParamAsObject(JIUnsignedInteger.class, JIFlags.FLAG_REPRESENTATION_UNSIGNED_INT);
+        return new JILocalMethodDescriptor("notifyAlarmAcknowledged", params);
+    }
+
+    private static void logCardEvent(IJIComObject ftc) throws Exception {
         JICallBuilder cb = new JICallBuilder(true);
         cb.setOpnum(1);
         cb.addInParamAsInt(4, JIFlags.FLAG_NULL);         // lEventType
         cb.addInParamAsInt(1, JIFlags.FLAG_NULL);         // lEventID
-        cb.addInParamAsDouble(0, JIFlags.FLAG_NULL);      // dEventTime
-        cb.addInParamAsBoolean(false, JIFlags.FLAG_NULL); // bHasRestoral
+        cb.addInParamAsDouble(40967.6424503935, JIFlags.FLAG_NULL);      // dEventTime
+        cb.addInParamAsInt(0, JIFlags.FLAG_NULL);         // bHasRestoral
         cb.addInParamAsInt(12345, JIFlags.FLAG_NULL);     // lCardNumber
         cb.addInParamAsInt(11111, JIFlags.FLAG_NULL);     // lFacilityCode
         cb.addInParamAsString("ES01", JIFlags.FLAG_REPRESENTATION_STRING_BSTR);    // sSystemId
         cb.addInParamAsString("ESI01", JIFlags.FLAG_REPRESENTATION_STRING_BSTR);   // sItemID
         cb.addInParamAsString("message", JIFlags.FLAG_REPRESENTATION_STRING_BSTR); // sMessage
         cb.addInParamAsString("details", JIFlags.FLAG_REPRESENTATION_STRING_BSTR); // sDetails
+        try {
+            ftc.call(cb);
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+
+    private static void notifyStatus(IJIComObject ftc) throws Exception {
+        JICallBuilder cb = new JICallBuilder(true);
+        cb.setOpnum(3);
+        cb.addInParamAsString("ES01", JIFlags.FLAG_REPRESENTATION_STRING_BSTR);    // sSystemId
+        cb.addInParamAsString("ESI01", JIFlags.FLAG_REPRESENTATION_STRING_BSTR);   // sItemID
+        cb.addInParamAsInt(0, JIFlags.FLAG_NULL);         // lState
+        cb.addInParamAsBoolean(false, JIFlags.FLAG_NULL); // bTampered
+        cb.addInParamAsBoolean(false, JIFlags.FLAG_NULL); // bOffline
+        cb.addInParamAsString("", JIFlags.FLAG_REPRESENTATION_STRING_BSTR);   // sMessage
         ftc.call(cb);
-
-//        startDetecting(reader, (r, report) -> report.getTags().forEach(t -> {
-//            log.info("Detected tag: {}", t);
-//        }));
-
     }
 
     private static ImpinjReader connectToReader(HostAndPort readerEndpoint) throws OctaneSdkException {
