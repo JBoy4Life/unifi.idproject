de15a82bf0dc467674efe5c6a1168c02ff4038c3
Leo Esaki <leo.esaki3@gmail.com>
2018-02-26 23:38:28 +0900

    Ensure change password page is accessible only when logged in

--- unifi-web/src/main/routes.js
+++ unifi-web/src/main/routes.js
@@ -19,6 +19,8 @@ const ProtectedRoutes = userIsAuthenticatedRedir(() => (
     <CrumbRoute exact path={ROUTES.MY_ACCOUNT} title="My Account" component={MyAccount} />
     <CrumbRoute exact path={ROUTES.DIRECTORY} title="Discovery" component={Discovery} />
 
+    <CrumbRoute exact path={ROUTES.CHANGE_PASSWORD} title="Change Password" component={ChangePassword} />
+
     <CrumbRoute path={ROUTES.EVACUATION} title="Evacuation" component={Evacuation} />
     <CrumbRoute path={ROUTES.ATTENDANCE} title="Attendance" component={Attendance} />
 
@@ -41,7 +43,6 @@ export default ({ history }) => (
     <Switch>
       <CrumbRoute exact path={ROUTES.LOGIN} title="Login" component={Login} />
       <CrumbRoute exact path={ROUTES.ACCEPT_INVITATION} title="Accept Invitation" component={ResetPassword} />
-      <CrumbRoute exact path={ROUTES.CHANGE_PASSWORD} title="Change Password" component={ChangePassword} />
       <CrumbRoute exact path={ROUTES.FORGOT_PASSWORD} title="Forgot Password" component={ForgotPassword} />
       <CrumbRoute exact path={ROUTES.CANCEL_PASSWORD_RESET} title="Cancel Password Request" component={CancelPasswordReset} />
       <CrumbRoute exact path={ROUTES.RESET_PASSWORD} title="Reset Password" component={ResetPassword} />
