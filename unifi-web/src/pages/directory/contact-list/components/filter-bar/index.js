51308970497b660c97ff117e7d7e87ca8aa5315b
Leo Esaki <leo.esaki3@gmail.com>
2018-03-01 05:30:52 +0900

    Disable Add contact link until implemented

--- unifi-web/src/pages/directory/contact-list/components/filter-bar/index.js
+++ unifi-web/src/pages/directory/contact-list/components/filter-bar/index.js
@@ -60,9 +60,9 @@ class FilterBar extends Component {
           />
         </Col>
         <Col xs={12} md={7}>
-          <Link to={ROUTES.DIRECTORY_NEW_HOLDER}>
+          {/*<Link to={ROUTES.DIRECTORY_NEW_HOLDER}>
             <Icon type="plus-circle-o" /> Add Contact
-          </Link>
+          </Link>*/}
         </Col>
         <Col xs={12} md={7} className="text-right">
           <Checkbox
