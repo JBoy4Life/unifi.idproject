3bc3899c69826da21da459969347bed77d25d89d
Michal Čudrnák <m@vslice.co.uk>
2018-04-26 00:22:32 +0100

    More plumbing, Java 10

--- unifi-gallagher-detection-logger/src/main/java/id/unifi/service/demo/gallagher/GallagherDemo.java
+++ unifi-gallagher-detection-logger/src/main/java/id/unifi/service/demo/gallagher/GallagherDemo.java
@@ -1,8 +1,16 @@
 package id.unifi.service.demo.gallagher;
 
+import com.codahale.metrics.MetricRegistry;
+import com.statemachinesystems.envy.Envy;
+import com.statemachinesystems.envy.Prefix;
+import id.unifi.service.common.config.HostAndPortValueParser;
+import id.unifi.service.common.config.MqConfig;
+import id.unifi.service.common.config.UnifiConfigSource;
 import id.unifi.service.common.detection.DetectableType;
 import id.unifi.service.common.detection.Detection;
 import id.unifi.service.common.types.pk.DetectablePK;
+import id.unifi.service.common.util.MetricUtils;
+import id.unifi.service.common.version.VersionInfo;
 import id.unifi.service.provider.security.gallagher.FtcApi;
 import id.unifi.service.provider.security.gallagher.IFTMiddleware2;
 import org.slf4j.Logger;
@@ -14,6 +22,11 @@ import java.time.ZoneId;
 import java.util.concurrent.CountDownLatch;
 
 public class GallagherDemo implements IFTMiddleware2 {
+    static {
+        // Prefer IPv4, otherwise 0.0.0.0 gets interpreted as IPv6 broadcast
+        System.setProperty("java.net.preferIPv4Stack", "true");
+    }
+
     private static final Logger log = LoggerFactory.getLogger(GallagherDemo.class);
     public static final ZoneId ZONE = ZoneId.systemDefault();
 
@@ -25,7 +38,27 @@ public class GallagherDemo implements IFTMiddleware2 {
         this.ftcApi = ftcApi;
     }
 
+    @Prefix("unifi")
+    private interface Config {
+        MqConfig mq();
+    }
+
     public static void main(String[] args) throws Exception {
+        Thread.setDefaultUncaughtExceptionHandler((t, e) -> {
+            LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME)
+                    .error("Uncaught exception in thread '" + t.getName() + "'", e);
+            System.exit(1);
+        });
+
+        log.info("Starting unifi.id Gallagher detection logger");
+        VersionInfo.log();
+
+        var config = Envy.configure(Config.class, UnifiConfigSource.get(), HostAndPortValueParser.instance);
+
+        var registry = new MetricRegistry();
+        var jmxReporter = MetricUtils.createJmxReporter(registry);
+        jmxReporter.start();
+
         log.info("Zone: {}", ZONE);
 
         GallagherDemo demo = setUp();
