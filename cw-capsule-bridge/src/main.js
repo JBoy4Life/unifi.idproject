ec77e5d627d8bf0620d1dbe281eddb8a35be9407
Andrew Steel <dev@andrewsteel.net>
2018-05-09 16:18:57 +0100

    Moved configuration to environment variables

--- cw-capsule-bridge/src/main.js
+++ cw-capsule-bridge/src/main.js
@@ -1,6 +1,24 @@
 import Capsule from "./capsule";
 import UnifiWsClient from "./lib/unifi-ws-client";
 
+//Get environment variables and define defaults:
+var env = require("env-variable")({
+    CAPSULE_BRIDGE_CLIENT_ID: "test-club",
+    CAPSULE_BRIDGE_OPERATOR_USERNAME: "test",
+    CAPSULE_BRIDGE_OPERATOR_PASSWORD: "test",
+    CAPSULE_BRIDGE_WEBSOCKET_IP: "127.0.0.1",
+    CAPSULE_BRIDGE_WEBSOCKET_PORT: "8000"
+});
+
+var config = {
+    clientId: env.CAPSULE_BRIDGE_CLIENT_ID,
+    operatorUsername: env.CAPSULE_BRIDGE_OPERATOR_USERNAME,
+    operatorPassword: env.CAPSULE_BRIDGE_OPERATOR_PASSWORD,
+    websocketIp: env.CAPSULE_BRIDGE_WEBSOCKET_IP,
+    websocketPort: env.CAPSULE_BRIDGE_WEBSOCKET_PORT
+};
+console.debug(`Configuration: ${JSON.stringify(config)}`);
+
 const CAPSULE_API_KEY = "2CHDNqokGLBzmYXUa6Ce62S2WhT26OhzJnsOl7bnQZ0YWsU5J3GN4yzEHY/h6ywK";
 
 async function fullSync() {
@@ -12,7 +30,7 @@ async function fullSync() {
 
     // Connect to the Unifi.id service.
     let unifi = new UnifiWsClient({
-        url: "ws://192.168.0.23:8000/service/json",
+        url: `ws://${config.websocketIp}:${config.websocketPort}/service/json`,
         type: "json"
     });
     await unifi.connect();
@@ -22,9 +40,9 @@ async function fullSync() {
     unifi.request({
         "messageType": "core.operator.auth-password",
         "payload": {
-            "clientId": "test-club",
-            "username": "test",
-            "password": "test"
+            "clientId": config.clientId,
+            "username": config.operatorUsername,
+            "password": config.operatorPassword
         }
     }, (authResponse) => {
         console.debug(authResponse);
@@ -32,7 +50,7 @@ async function fullSync() {
             unifi.request({
                 "messageType": "core.holder.add-holder",
                 "payload": {
-                    "clientId": "test-club",
+                    "clientId": config.clientId,
                     "clientReference": person.id.toString(),
                     "holderType": "contact",
                     "name": person.firstName + " " + person.lastName,
