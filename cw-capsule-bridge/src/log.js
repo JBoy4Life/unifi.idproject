b00f7be5246169913f490c369273392d3288ba72
Andrew Steel <dev@andrewsteel.net>
2018-06-19 14:54:35 +0100

    Removed leading space from log messages (as `systemd` removes the log level notations)

--- cw-capsule-bridge/src/log.js
+++ cw-capsule-bridge/src/log.js
@@ -1,23 +1,23 @@
 function Log() {}
 
 Log.debug = function (message) {
-    // console.debug(`<7> [DEBUG] ${message}`);
+    // console.debug(`<7>[DEBUG] ${message}`);
 };
 
 Log.error = function (message) {
-    console.error(`<3> [ERROR] ${message}`);
+    console.error(`<3>[ERROR] ${message}`);
 };
 
 Log.info = function (message) {
-    console.info(`<6> [INFO] ${message}`);
+    console.info(`<6>[INFO] ${message}`);
 };
 
 Log.notice = function (message) {
-    console.log(`<5> [NOTICE] ${message}`);
+    console.log(`<5>[NOTICE] ${message}`);
 };
 
 Log.warning = function (message) {
-    console.warn(`<4> [WARNING] ${message}`);
+    console.warn(`<4>[WARNING] ${message}`);
 };
 
 export default Log;
